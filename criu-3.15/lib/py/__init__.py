bb33eb72c307db39de7abebda49c4bdbbe1fd541
abhishek-synth <abhishek@synth>
2021-10-19 17:38:24 -0400

    refactor: begin

--- criu-3.15/lib/py/__init__.py
+++ criu-3.15/lib/py/__init__.py
@@ -1,7 +1,3 @@
 from . import rpc_pb2 as rpc
 from . import images
-from . import utils
-from . import elf_utils
-from . import stack_map_utils
 from .criu import *
-from .transformation import stack_transform
--- criu-3.15/lib/py/transformation/__init__.py
+++
@@ -1,5 +0,0 @@
-from . import definitions
-from . import reg_aarch64
-from . import reg_x86_64
-from pycriu import stack_map_utils
-from pycriu import elf_utils
