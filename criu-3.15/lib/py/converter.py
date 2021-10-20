7498a605bd438fbb52ac65122f5206c26ab4db58
abhishek-synth <abhishek@synth>
2021-10-20 17:26:31 -0400

    refactor: handled files which do not need trans logic

--- criu-3.15/lib/py/converter.py
+++ criu-3.15/lib/py/converter.py
@@ -1,4 +1,11 @@
-# Current version by Abhishek Bapat, Virginia Tech 2021.
+# Current version by Abhishek Bapat. SSRG, Virginia Tech 2021.
+# abapat28@vt.edu
+
+X8664_SUFFIX = '_x86-64'
+AARCH64_SUFFIX = '_aarch64'
+
+AARCH64 = 'AARCH64'
+X8664 = 'X86_64'
 
 CGROUP = 'cgroup' 
 CORE = 'core' 
@@ -14,7 +21,6 @@ SECCOMP = 'seccomp'
 TIMENS = 'timens'
 TTYINFO = 'tty-info'
 
-from typing import AbstractSet
 import pycriu
 import sys
 import os
@@ -25,7 +31,7 @@ from os import listdir
 from os.path import isfile, join
 
 
-class Converter():
+class Converter(): #TODO: Extend the logic for multiple PIDs
     __metaclass__ = ABCMeta
     def __init__(self, src_dir, dest_dir, bin, debug):
         assert os.path.exists(src_dir), "Source directory does not exist"
@@ -37,65 +43,58 @@ class Converter():
         self.src_dir = src_dir
         self.dest_dir = dest_dir
         self.bin = bin
-        self.mult_entry_files = [
-            'cgroup', 
-            'core', 
-            'fdinfo',
-            'files', 
-            'fs',
-            'ids',
-            'inventory',
-            'mm',
-            'pagemap',
-            'pstree',
-            'seccomp',
-            'timens',
-            'tty-info'
+        self.entry_num = 0
+        self.files_per_pid = [
+            IDS,
+            MM,
+            CORE,
+            FS,
+            PAGEMAP,
         ]
         self.stack_page_offset = -1
-        self.src_image_file_names = dict()
-        self.dest_image_file_names = dict()
-        img_files = [f for f in listdir() if (isfile(join(src_dir, f)) and "img" in f)]
+        self.src_image_file_paths = dict()
+        self.dest_image_file_paths = dict()
+        img_files = [f for f in listdir(src_dir) if (isfile(join(src_dir, f)) and "img" in f)]
         for f in img_files:
             if CGROUP in f:
-                self.src_image_file_names[CGROUP] = join(src_dir, f)
-                self.dest_image_file_names[CGROUP] = join(dest_dir, f)
+                self.src_image_file_paths[CGROUP] = join(src_dir, f)
+                self.dest_image_file_paths[CGROUP] = join(dest_dir, f)
             if CORE in f:
-                self.src_image_file_names[CORE] = join(src_dir, f)
-                self.dest_image_file_names[CORE] = join(dest_dir, f)
+                self.src_image_file_paths[CORE] = join(src_dir, f)
+                self.dest_image_file_paths[CORE] = join(dest_dir, f)
             if FDINFO in f:
-                self.src_image_file_names[FDINFO] = join(src_dir, f)
-                self.dest_image_file_names[FDINFO] = join(dest_dir, f)
+                self.src_image_file_paths[FDINFO] = join(src_dir, f)
+                self.dest_image_file_paths[FDINFO] = join(dest_dir, f)
             if FILES in f:
-                self.src_image_file_names[FILES] = join(src_dir, f)
-                self.dest_image_file_names[FILES] = join(dest_dir, f)
+                self.src_image_file_paths[FILES] = join(src_dir, f)
+                self.dest_image_file_paths[FILES] = join(dest_dir, f)
             if FS in f:
-                self.src_image_file_names[FS] = join(src_dir, f)
-                self.dest_image_file_names[FS] = join(dest_dir, f)
+                self.src_image_file_paths[FS] = join(src_dir, f)
+                self.dest_image_file_paths[FS] = join(dest_dir, f)
             if IDS in f:
-                self.src_image_file_names[IDS] = join(src_dir, f)
-                self.dest_image_file_names[IDS] = join(dest_dir, f)
+                self.src_image_file_paths[IDS] = join(src_dir, f)
+                self.dest_image_file_paths[IDS] = join(dest_dir, f)
             if INVENTORY in f:
-                self.src_image_file_names[INVENTORY] = join(src_dir, f)
-                self.dest_image_file_names[INVENTORY] = join(dest_dir, f)
+                self.src_image_file_paths[INVENTORY] = join(src_dir, f)
+                self.dest_image_file_paths[INVENTORY] = join(dest_dir, f)
             if MM in f:
-                self.src_image_file_names[MM] = join(src_dir, f)
-                self.dest_image_file_names[MM] = join(dest_dir, f)
+                self.src_image_file_paths[MM] = join(src_dir, f)
+                self.dest_image_file_paths[MM] = join(dest_dir, f)
             if PAGEMAP in f:
-                self.src_image_file_names[PAGEMAP] = join(src_dir, f)
-                self.dest_image_file_names[PAGEMAP] = join(dest_dir, f)
+                self.src_image_file_paths[PAGEMAP] = join(src_dir, f)
+                self.dest_image_file_paths[PAGEMAP] = join(dest_dir, f)
             if PSTREE in f:
-                self.src_image_file_names[PSTREE] = join(src_dir, f)
-                self.dest_image_file_names[PSTREE] = join(dest_dir, f)
+                self.src_image_file_paths[PSTREE] = join(src_dir, f)
+                self.dest_image_file_paths[PSTREE] = join(dest_dir, f)
             if SECCOMP in f:
-                self.src_image_file_names[SECCOMP] = join(src_dir, f)
-                self.dest_image_file_names[SECCOMP] = join(dest_dir, f)
+                self.src_image_file_paths[SECCOMP] = join(src_dir, f)
+                self.dest_image_file_paths[SECCOMP] = join(dest_dir, f)
             if TIMENS in f:
-                self.src_image_file_names[TIMENS] = join(src_dir, f)
-                self.dest_image_file_names[TIMENS] = join(dest_dir, f)
+                self.src_image_file_paths[TIMENS] = join(src_dir, f)
+                self.dest_image_file_paths[TIMENS] = join(dest_dir, f)
             if TTYINFO in f:
-                self.src_image_file_names[TTYINFO] = join(src_dir, f)
-                self.dest_image_file_names[TTYINFO] = join(dest_dir, f)
+                self.src_image_file_paths[TTYINFO] = join(src_dir, f)
+                self.dest_image_file_paths[TTYINFO] = join(dest_dir, f)
 
 
     def log(self, *args):
@@ -122,8 +121,12 @@ class Converter():
             sys.exit(1)
         return img
     
-    def get_pid(self, p, val):
-        return p[val] if val in p else p['ns_' + val][0]
+    def get_all_pids(self, pstree_file):
+        all_pids=list()
+        pgm_img=self.load_image_file(pstree_file)
+        for entry in pgm_img["entries"]:
+            all_pids.append(entry["pid"])
+        return all_pids
     
     def get_pages_id(self, pm_full_path):
         page_map = self.load_image_file(pm_full_path)
@@ -145,7 +148,7 @@ class Converter():
             else:
                 break
         assert pages_to_skip != 0 and st_vaddr != 0 and end_vaddr !=0, \
-            "something went wrong reading stack"
+            "something went wrong computing stack offset"
         self.stack_page_offset = (pages_to_skip << 12) + (sp - st_vaddr)
         self.log('Stack Offset: ', self.stack_page_offset)
         return self.stack_page_offset
@@ -155,32 +158,60 @@ class Converter():
         pass
 
     @abstractmethod
+    def assert_conditions(self):
+        pass
+
     def transform_cgroup_file(self):
-        pass
+        if CGROUP not in self.src_image_file_paths:
+            return
+        src_cgroup = self.src_image_file_paths[CGROUP]
+        dst_cgroup = self.dest_image_file_paths[CGROUP]
+        shutil.copyfile(src_cgroup, dst_cgroup)
+        self.log('Copied cgroup file')
 
-    @abstractmethod
     def transform_fdinfo_file(self):
-        pass
+        if FDINFO not in self.src_image_file_paths:
+            return
+        src_fd = self.src_image_file_paths[FDINFO]
+        dst_fd = self.dest_image_file_paths[FDINFO]
+        shutil.copyfile(src_fd, dst_fd)
+        self.log('Copied fdinfo file')
 
-    @abstractmethod
     def transform_fs_file(self):
-        pass
+        if FS not in self.src_image_file_paths:
+            return
+        src_fs = self.src_image_file_paths[FS]
+        dst_fs = self.dest_image_file_paths[FS]
+        shutil.copyfile(src_fs, dst_fs)
+        self.log('Copied fs file')
 
-    @abstractmethod
     def transform_inventory_file(self):
-        pass
+        if INVENTORY not in self.src_image_file_paths:
+            return
+        src_inventory = self.src_image_file_paths[INVENTORY]
+        dst_inventory = self.dest_image_file_paths[INVENTORY]
+        shutil.copyfile(src_inventory, dst_inventory)
+        self.log('Copied inventory file')
 
     @abstractmethod
     def transform_pagemap_file(self):
         pass
 
-    @abstractmethod
     def transform_pstree_file(self):
-        pass
+        if PSTREE not in self.src_image_file_paths:
+            return
+        src_ps = self.src_image_file_paths[PSTREE]
+        dst_ps = self.dest_image_file_paths[PSTREE]
+        shutil.copyfile(src_ps, dst_ps)
+        self.log('Copied pstree file')
 
-    @abstractmethod
     def transform_ttyinfo_file(self):
-        pass
+        if TTYINFO not in self.src_image_file_paths:
+            return
+        src_tty = self.src_image_file_paths[TTYINFO]
+        dst_tty = self.dest_image_file_paths[TTYINFO]
+        shutil.copyfile(src_tty, dst_tty)
+        self.log('Copied tty-info file')
 
     @abstractmethod
     def transform_core_file(self): #regs
@@ -190,21 +221,33 @@ class Converter():
     def transform_files_file(self):
         pass
 
-    @abstractmethod
     def transform_ids_file(self):
-        pass
+        if IDS not in self.src_image_file_paths:
+            return
+        src_ids = self.src_image_file_paths[IDS]
+        dst_ids = self.dest_image_file_paths[IDS]
+        shutil.copyfile(src_ids, dst_ids)
+        self.log('Copied ids file')
 
     @abstractmethod
     def transform_mm_file(self):
         pass
 
-    @abstractmethod
     def transform_seccomp_file(self):
-        pass
+        if SECCOMP not in self.src_image_file_paths:
+            return
+        src_seccomp = self.src_image_file_paths[SECCOMP]
+        dst_seccomp = self.dest_image_file_paths[SECCOMP]
+        shutil.copyfile(src_seccomp, dst_seccomp)
+        self.log('Copied seccomp file')
 
-    @abstractmethod
     def transform_timens_file(self):
-        pass
+        if TIMENS not in self.src_image_file_paths:
+            return
+        src_timens = self.src_image_file_paths[TIMENS]
+        dst_timens = self.dest_image_file_paths[TIMENS]
+        shutil.copyfile(src_timens, dst_timens)
+        self.log('Copied timens file')
 
     @abstractmethod
     def transform_pages_file(self): #stack, code_pages, vdso
@@ -229,3 +272,27 @@ class Converter():
         self.transform_pages_file()
         self.transform_seccomp_file()
         self.transform_timens_file()
+
+
+class Aarch64Converter(Converter):
+    def __init__(self, src_dir, dest_dir, bin, debug):
+        Converter.__init__(self, src_dir, dest_dir, bin, debug)
+        self.arch = AARCH64
+
+    def assert_conditions(self): # call before calling recode
+        core_file = self.load_image_file(self.src_image_file_paths[CORE])
+        entry = self.entry_num
+        arch = core_file['entries'][entry]['mtype']
+        assert arch != AARCH64, "Same src and dest arch do not need transformation"
+    
+    def copy_bin_files(self):
+        x86_bin = join(self.src_dir, self.bin+X8664_SUFFIX)
+        aarch64_bin = join(self.src_dir, self.bin+AARCH64_SUFFIX)
+        base = join(self.dest_dir, self.bin)
+        x86_bin_cp = join(self.dest_dir, self.bin+X8664_SUFFIX)
+        aarch64_bin_cp = join(self.dest_dir, self.bin+AARCH64_SUFFIX)
+        shutil.copyfile(x86_bin, x86_bin_cp)
+        shutil.copyfile(aarch64_bin, aarch64_bin_cp)
+        shutil.copyfile(aarch64_bin, base)
+        self.log('Binaries copied')
+    
