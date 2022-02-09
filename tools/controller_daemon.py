22fb17d29aea6b29b7ff81b84bb9bcda2b1c1e49
abhishek-synth <abhishek@synth>
2022-02-09 13:48:38 -0500

    wip: controller daemon

--- tools/controller_daemon.py
+++ tools/controller_daemon.py
@@ -1,69 +1,70 @@
 import subprocess
 import os
+import sys
+import time
 
 class ControllerDaemon:
-    def __init__(self, dir, bin, tranproc, print_stdout = True) -> None:
-        self.dir = dir
-        self.bin = bin
-        self.print_stdout = print_stdout
-        self.tranproc = tranproc
+    def __init__(self):
+        self.root_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+    
+    def _spawn_independent_subprocess(self, args, cwd=None):
+        try:
+            pid = os.fork()
+            if pid > 0:
+                #parent process, return and continue execution.
+                return
+        except OSError as e:
+            print("fork failed: %d, (%s)" % e.errno, e.strerror)
+        
+        # execution from here is only continued by the child process
+        # spawn the subprocess and exit
+        proc = subprocess.Popen(args,
+                            cwd=cwd,
+                            universal_newlines=True,
+                            preexec_fn=os.setpgrp)
+        sys.exit(0)
 
 
-    def assert_conditions(self):
-        assert os.path.exists(os.path.join(self.dir, self.bin))
-        assert os.path.exists(os.path.join(self.dir, "Makefile"))
-        bin_dir = os.path.join(self.dir, "bin/")
-        assert os.path.exists(bin_dir)
-        assert os.path.exists(os.path.join(bin_dir, self.bin+"_aarch64"))
-        assert os.path.exists(os.path.join(bin_dir, self.bin+"_x86-64"))
-        debugger = os.path.join(self.tranproc, "tools", "debugger")
-        assert os.path.exists(debugger)
+    def run(self, command, cwd):
+        self._spawn_independent_subprocess(command, cwd=cwd)
 
 
-    def run(self):
-        proc = subprocess.Popen(os.path.join(self.dir, self.bin),
-                            stdout=subprocess.PIPE,
-                            stderr=subprocess.PIPE,
-                            cwd=self.dir,
-                            universal_newlines=True)
-        stdout, stderr = proc.communicate()
-        if self.print_stdout:
-            print(stdout)
-
-
-    def run_and_infect(self, addr):
-        debugger = os.path.join(self.tranproc, "tools", "debugger")
-        proc = subprocess.Popen([debugger, self.bin, addr],
-                            stdout=subprocess.PIPE,
-                            stderr=subprocess.PIPE,
-                            cwd=self.dir,
-                            universal_newlines=True)
+    def run_and_infect(self, addr, bin, cwd):
+        debugger = os.path.join(self.root_dir, "tools", "debugger")
+        self._spawn_independent_subprocess([debugger, bin, addr], cwd=cwd)
     
 
-    def get_pid(self):
-        ps = subprocess.Popen(["ps", "-a"],
-                            stdout=subprocess.PIPE,
-                            stderr=subprocess.PIPE,
-                            universal_newlines=True)
-        stdo, stder = ps.communicate()
-        o = stdo.splitlines()
-        bin = [b for b in o if self.bin in b]
-        pid = bin[0].split(' ')
-        return pid[2]
+    def get_pid(self, bin):
+        return subprocess.check_output(["pidof", bin], universal_newlines=True)
     
 
-    def dump(self, pid):
-        proc = subprocess.Popen(["make", "PID=%s" % pid, "dump"],
-                            stdout=subprocess.PIPE,
-                            stderr=subprocess.PIPE,
-                            cwd=self.dir,
-                            universal_newlines=True)
+    def dump(self, pid, cwd):
+        self._spawn_independent_subprocess(["make", "PID=%s" % pid, "dump"], cwd=cwd)
+
+
+def assert_conditions(dir, bin, tranproc):
+    assert os.path.exists(os.path.join(dir, bin))
+    assert os.path.exists(os.path.join(dir, "Makefile"))
+    bin_dir = os.path.join(dir, "bin/")
+    assert os.path.exists(bin_dir)
+    assert os.path.exists(os.path.join(bin_dir, bin+"_aarch64"))
+    assert os.path.exists(os.path.join(bin_dir, bin+"_x86-64"))
+    debugger = os.path.join(tranproc, "tools", "debugger")
+    assert os.path.exists(debugger)
 
 
 addr = "0x50146f"
-cd = ControllerDaemon("/home/abhishek/temp/snu_npb/bt/temp/", "bt", "/home/abhishek/projects/TranProc/")
-cd.assert_conditions()
-cd.run_and_infect(addr)
-pid = cd.get_pid()
-cd.dump(pid)
-print('dumped')
+dir = "/home/abhishek/temp/snu_npb/bt/temp/"
+bin = "bt"
+tranproc = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+assert_conditions(dir, bin, tranproc)
+cd = ControllerDaemon()
+# cd.run(os.path.join(dir, bin), dir)
+cd.run_and_infect(addr, bin, dir)
+time.sleep(1)
+pid = cd.get_pid(bin)
+print("PID: %s", pid)
+#cd.dump(pid, dir)
+print('Parent process ends')
