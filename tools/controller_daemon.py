5d630b5518cf2279cec4502184cd0e8afda90456
abhishek-synth <abhishek@synth>
2022-02-10 17:44:43 -0500

    feat: first draft controller daemon, scp feature left

--- tools/controller_daemon.py
+++ tools/controller_daemon.py
@@ -1,14 +1,22 @@
-from curses.ascii import DEL
+import getopt
 import subprocess
 import os
 import sys
 import time
+import Pyro4
+import platform
 
 
-DELAY = 0.1 #TODO: find out a better way.
 DELAY_PRECISION = 0.01
 
 
+NAME_PREFIX = "stack_pop.controller_daemon."
+CPU_ARCH = platform.processor()
+SERVER_NAME = NAME_PREFIX + CPU_ARCH
+
+
+@Pyro4.expose
+@Pyro4.behavior(instance_mode="single")
 class ControllerDaemon:
     def __init__(self):
         self.root_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -94,13 +102,13 @@ class ControllerDaemon:
             ], cwd=dir)
     
 
-    def restore(self, bin, cwd, pid):
+    def restore(self, bin, cwd):
         self._spawn_independent_subprocess(["make", "BIN=%s" % bin, "restore"], cwd=cwd)
     
 
     def restore_and_infect(self, bin, cwd, pid, addr):
         attach_pid = os.path.join(self.root_dir, "tools", "attach_pid")
-        self.restore(bin, cwd, pid)
+        self.restore(bin, cwd)
         pid2 = self.check_pid(bin)
         self._spawn_independent_subprocess([attach_pid, pid, addr], cwd=cwd)
 
@@ -109,35 +117,46 @@ class ControllerDaemon:
         self._spawn_independent_subprocess(["kill", "-SIGCONT", "%s" % pid])
 
 
-def assert_conditions(dir, bin, tranproc):
-    assert os.path.exists(os.path.join(dir, bin))
-    assert os.path.exists(os.path.join(dir, "Makefile"))
-    bin_dir = os.path.join(dir, "bin/")
-    assert os.path.exists(bin_dir)
-    assert os.path.exists(os.path.join(bin_dir, bin+"_aarch64"))
-    assert os.path.exists(os.path.join(bin_dir, bin+"_x86-64"))
-    debugger = os.path.join(tranproc, "tools", "debugger")
-    assert os.path.exists(debugger)
-    attach_pid = os.path.join(tranproc, "tools", "attach_pid")
-    assert os.path.exists(attach_pid)
+def main():
+    Pyro4.Daemon.serveSimple(
+        {
+            ControllerDaemon: SERVER_NAME
+        },
+        ns=True
+    )
 
 
-addr = "0x50146f"
-addr2 = "0x50193f"
-dir = "/root/bt"
-bin = "bt"
-tranproc = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+if __name__ == "__main__":
+    main()
 
-# assert_conditions(dir, bin, tranproc)
-cd = ControllerDaemon()
-# cd.run(os.path.join(dir, bin), dir)
-cd.run_and_infect(addr, bin, dir)
-# time.sleep(DELAY)
-pid = cd.check_pid(bin)
-cd.dump(pid, dir)
-cd.check_killed(bin)
-cd.transform(bin, "aarch64", dir)
-# cd.restore(bin, dir, pid)
-cd.restore_and_infect(bin, dir, pid, addr2)
+# def assert_conditions(dir, bin, tranproc):
+#     assert os.path.exists(os.path.join(dir, bin))
+#     assert os.path.exists(os.path.join(dir, "Makefile"))
+#     bin_dir = os.path.join(dir, "bin/")
+#     assert os.path.exists(bin_dir)
+#     assert os.path.exists(os.path.join(bin_dir, bin+"_aarch64"))
+#     assert os.path.exists(os.path.join(bin_dir, bin+"_x86-64"))
+#     debugger = os.path.join(tranproc, "tools", "debugger")
+#     assert os.path.exists(debugger)
+#     attach_pid = os.path.join(tranproc, "tools", "attach_pid")
+#     assert os.path.exists(attach_pid)
 
-print('Parent process ends')
+
+# addr = "0x50146f"
+# addr2 = "0x50193f"
+# dir = "/root/bt"
+# bin = "bt"
+# tranproc = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+# # assert_conditions(dir, bin, tranproc)
+# cd = ControllerDaemon()
+# # cd.run(os.path.join(dir, bin), dir)
+# cd.run_and_infect(addr, bin, dir)
+# pid = cd.check_pid(bin)
+# cd.dump(pid, dir)
+# cd.check_killed(bin)
+# cd.transform(bin, "aarch64", dir)
+# # cd.restore(bin, dir, pid)
+# cd.restore_and_infect(bin, dir, pid, addr2)
+
+# print('Parent process ends')
