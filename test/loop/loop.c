394f382cb3dd92e9023a3315d375f5f02aafae59
abhishek-synth <abhishek@synth>
2022-05-03 12:40:28 -0400

    fix: simplified loop example

--- test/loop/loop.c
+++ test/loop/loop.c
@@ -1,59 +1,36 @@
 #include <stdio.h>
 #include <unistd.h>
-#include <signal.h>
 #include <stdlib.h>
 
-void func_path_a(int cnt)
+#include "migrate.h"
+
+int pid_main;
+
+void func2(int i)
 {
-    int i = 0;
-    printf("In function %s.\n", __func__);
-    while(i < cnt) {
-        sleep(5);
-        printf("%d \n", ++i);
-    }
-    printf("Finish %s.\n", __func__);
+    printf("[%d] Executing %s on: ", i, __func__);
+    fflush(stdout);
+    system("uname -m");
 }
 
-void func_path_b(int cnt)
+void func1(int i)
 {
-    printf("In function %s.\n", __func__);
-    func_path_a(cnt);
-    printf("Finish %s.\n", __func__);
+    printf("[%d] Executing %s on: ", i, __func__);
+    fflush(stdout);
+    system("uname -m");
+    func2(i);
+    sleep(2);
 }
 
-/**
- * @brief Use ./multi_path -a | -b | -n <loop cnt>
- */
 int main(int argc, char *argv[])
 {
-    int cnt = 5;
-    int flags=2, opt;
+    int i=0;
 
-    /**
-     * @brief Simulate a multi-option program.
-     */
-    while ((opt = getopt(argc, argv, "abn:")) != -1) {
-        switch (opt) {
-        case 'a':
-            flags = 1;
-            break;
-        case 'b':
-            flags = 2;
-            break;
-        case 'n':
-            cnt = atoi(optarg);
-            break;
-        default: /* '?' */
-            fprintf(stderr, "Usage: %s [-n loop_cnt] [-a] [-b]\n",
-                    argv[0]);
-            exit(EXIT_FAILURE);
-        }
+    pid_main = getpid();
+    printf("pid: %d.\n", pid_main);
+
+    while (1) {
+        func1(++i);
     }
-    printf("pid: %d, cnt: %d \n", getpid(), cnt);
-
-    if (flags == 1) func_path_a(cnt);
-    if (flags == 2) func_path_b(cnt);
-
-    printf("%s: Finish the loop...\n", __func__);
     return 0;
 }
