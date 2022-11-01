f3771835395366c3c7c2d5b60b23ff8bc0970fda
abhishek-synth <abhishek@synth>
2022-11-01 15:34:29 -0400

    chore: redis makefile

---
+++ test/redis/setup_x86.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+X86=_x86-64
+ARM=_aarch64
+
+USER=$(whoami)
+
+BASE=/home/$USER
+
+i=redis-server
+rm -rf $BASE/$i
+mkdir $BASE/$i
+mkdir $BASE/$i/bin
+cp $i/$i$X86 $BASE/$i/bin/.
+cp $i/$i$ARM $BASE/$i/bin/.
+cp $i/$i$X86 $BASE/$i/$i
+cp $i/Makefile $BASE/$i/.
